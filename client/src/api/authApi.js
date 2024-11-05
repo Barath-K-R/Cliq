dd8edc0af636992941e78576cfb345343b35227f
Barath-K-R <barath223637@gmail.com>
2024-11-05 10:07:49 +0530

    authapi.js conflict resolved

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,10 +1,3 @@
-import axios from 'axios'
+import axios from "axios";
 
-<<<<<<< Updated upstream
-const userApi=axios.create({baseURL:"http://localhost:5000"});
-
-export const loginUser=(formdata)=>userApi.post('/user/login',formdata);
-=======
 export const loginUser = (formdata) => authApi.post("/login", formdata);
-
->>>>>>> Stashed changes
